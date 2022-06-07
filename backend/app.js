c491726f1c18901955285005b5328fb3b3453039
Pulkit Agrawal <48528949+Pulkit426@users.noreply.github.com>
2022-06-07 23:36:02 +0530

    add SPA serve

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+const path = require('path');
 const cors = require('cors')
 const mongoose = require('mongoose')
 require('express-async-errors')
@@ -34,6 +35,12 @@ if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
+
+app.use(express.static(path.resolve(__dirname, 'build', 'index.html')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
+    });
 
 app.use(errorHandler)
 
